fcbc280ca9d5046a13fc3d0c230f79ab37a9e401
Joris Ballemans <jorisballemans@frederic.home>
2025-05-26 22:44:46 +0200

    Added avg_runs graph

---
+++ project/avg_runs.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.stats import sem, t
+import os
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg') 
+from matplotlib import pyplot as plt
+
+def calculate_mean_ci(data_dir = "data", ci = 0.95):
+    data_files = [os.path.join(data_dir, f) for f in os.listdir("data") if f.endswith(".csv")]
+    all_runs = [pd.read_csv(f) for f in data_files]
+    average_run = pd.concat(all_runs).groupby(level=0).mean().reset_index(drop=True)
+    runs_stack = np.stack([df.values for df in all_runs])
+
+    mean_runs = np.mean(runs_stack, axis=0)
+    confidence_intervals = sem(runs_stack, axis=0) * t.ppf((1 + ci) / 2, len(all_runs) - 1)
+    return average_run, mean_runs, confidence_intervals
+
+avg_run, mean_runs, ci_runs = calculate_mean_ci(ci = 0.95)
+
+x = range(mean_runs.shape[0])
+for index, col_name in enumerate(avg_run.columns):
+    mean_run = mean_runs[:, index]
+    ci_run = ci_runs[:, index]
+    plt.plot(x, mean_run, label=f"{col_name} mean")
+    plt.fill_between(x, mean_run - ci_run, mean_run + ci_run, alpha=0.3, label=f"{col_name} 95% CI")
+
+plt.title("Mean and 95% Confidence Interval")
+plt.xlabel("Generation")
+plt.ylabel("Population size")
+plt.legend(loc="lower right")
+plt.show()
